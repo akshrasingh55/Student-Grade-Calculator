d5463644923f5347ad486801d51bcbdc9806cbd7
akshrasingh55 <chaudhary.25mib10016@vitbhopal.ac.in>
2025-11-23 23:54:59 +0530

    Create gradecalculator.py

---
+++ gradecalculator.py
@@ -0,0 +1,44 @@
+
+
+print("Student Grade Calculator")
+
+total = 0
+marks = []
+
+for i in range(1, 6):
+    while True:
+        try:
+            m = float(input(f"Enter marks for Subject {i}: "))
+            if m < 0 or m > 100:
+                print("Please enter a valid mark (0–100).")
+                continue
+            marks.append(m)
+            total += m
+            break
+        except ValueError:
+            print("Invalid input. Enter a number only.")
+
+average = total / 5
+
+if average >= 90:
+    grade = "A"
+    feedback = "Excellent performance!"
+elif average >= 75:
+    grade = "B"
+    feedback = "Good performance!"
+elif average >= 60:
+    grade = "C"
+    feedback = "Satisfactory performance."
+elif average >= 40:
+    grade = "D"
+    feedback = "Need to improve."
+else:
+    grade = "F"
+    feedback = "Fail. Work harder next time."
+
+# Display Output
+print("\n Result")
+print("Total Marks:", total)
+print("Average Marks:", average)
+print("Grade:", grade)
+print("Feedback:", feedback)
